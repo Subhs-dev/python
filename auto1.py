050db1e16ffb1351b24db174b9713227df4d02f7
Subham Neupane <subhamneupane37@gmail.com>
2024-06-25 22:24:25 +0545

    subham

---
+++ auto1.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+
+
+def greet(name):
+    msg=f" Good Morning ,{name}"
+    return msg
+
+gname=['Roman','Sandip','Subham','Bikesh']
+for name in gname:
+    greeting=greet(name)
+    time.sleep(3)
+    pyautogui.write(greeting)
+    pyautogui.press("enter")
